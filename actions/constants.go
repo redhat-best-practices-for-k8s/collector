e203d470a5e2e294d43399d3249032c1d229b9f8
Shir Moran <shmoran@shmoran.tlv.csb>
2023-05-30 12:33:32 +0300

    made linting changes

--- actions/constants.go
+++ actions/constants.go
@@ -1,24 +1,25 @@
 package actions
 
 // parser.go constants
-const CLAIM_TAG = "claim"
-const VERSIONS_TAG = "versions"
-const METADATA_TAG = "metadata"
-const RESULTS_TAG = "results"
-const CLAIM_FILE_INPUT_NAME = "claimFile"
-const CREATED_BY_INPUT_NAME = "created_by"
-const PARTNER_NAME_INPUT_NAME = "partner_name"
+const ClaimTag = "claim"
+const VersionsTag = "versions"
+const ResultsTag = "results"
+const ClaimFileInputName = "claimFile"
+const CreatedByInputName = "created_by"
+const PartnerNameInputName = "partner_name"
 
-const USE_COLLECTOR_SQL_CMD = `USE cnf; `
-const INSERT_TO_CLAIM_SQL_CMD = `INSERT INTO claim 
+const UseCollectorSQLCmd = `USE cnf; `
+const InsertToClaimSQLCmd = `INSERT INTO claim 
 								(cnf_version, created_by, upload_time, partner_name)
 								VALUES (?, ?, ?, ?);`
-const INSERT_TO_CLAIM_RES_SQL_CMD = `INSERT INTO claim_result
+const InsertToClaimResSQLCmd = `INSERT INTO claim_result
 							(claim_id, suite_name, test_id, test_status)
 							VALUES (?, ?, ?, ?);`
-const EXTRACT_LAST_CLAIM_ID = `SELECT id FROM cnf.claim ORDER BY id DESC LIMIT 1;`
+const ExtractLastClaimID = `SELECT id FROM cnf.claim ORDER BY id DESC LIMIT 1;`
+const ParseLowerBound = 10
+const ParseUpperBound = 20
 
 // results.go constants
-const SELECT_ALL_FROM_CLAIM = "SELECT * FROM cnf.claim"
-const SELECT_ALL_FROM_CLAIM_RESULT = "SELECT * FROM cnf.claim_result"
-const RESULT_JSON_PATH = "results.json"
+const SelectAllFromClaim = "SELECT * FROM cnf.claim"
+const SelectAllFromClaimResult = "SELECT * FROM cnf.claim_result"
+const ResultJSONPath = "results.json"
