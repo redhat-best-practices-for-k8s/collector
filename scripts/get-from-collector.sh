adbcf37e29e8d4a1820325ebcc437d3952f296e2
Shir Moran <shmoran@redhat.com>
2023-10-31 17:37:41 +0200

    added args saftey check to GET and POST collector's scripts

--- scripts/get-from-collector.sh
+++ scripts/get-from-collector.sh
@@ -4,4 +4,9 @@ endpoint="$1"
 partnerName="$2"
 decodedPassword="$3"
 
+if [ -z "$endpoint" ] || [ -z "$partnerName" ] || [ -z "$decodedPassword"]; then
+    echo "Usage: $0 [ endpoint ] [ partner_name ] [ password ]"
+    exit 1
+fi
+
 curl "$endpoint?partner_name=$partnerName&decoded_password=$decodedPassword"
--- scripts/send-to-collector.sh
+++ scripts/send-to-collector.sh
@@ -6,6 +6,11 @@ executedBy="$3"
 partnerName="$4"
 decodedPassword="$5"
 
+if [ -z "$endpoint" ] || [ -z "$claimFile" ] || [ -z "$executedBy"]; then
+    echo "Usage: $0 [ endpoint ] [ path/to/claim.json ] [ executed_by ] [ partner_name(optional ] [ password(optinal) ]"
+    exit 1
+fi
+
 curl -X POST $endpoint \
     -H "Content-Type: multipart/form-data" \
     -F "claimFile=@$claimFile" \
