a36a52676c5581df863972b7193fa948cc8334e1
Shir Moran <101132224+shirmoran@users.noreply.github.com>
2023-11-01 16:36:40 +0200

    Update send-to-collector.sh missing space

--- scripts/send-to-collector.sh
+++ scripts/send-to-collector.sh
@@ -6,7 +6,7 @@ executedBy="$3"
 partnerName="$4"
 decodedPassword="$5"
 
-if [ -z "$endpoint" ] || [ -z "$claimFile" ] || [ -z "$executedBy"]; then
+if [ -z "$endpoint" ] || [ -z "$claimFile" ] || [ -z "$executedBy" ]; then
     echo "Usage: $0 [ endpoint ] [ path/to/claim.json ] [ executed_by ] [ partner_name(optional ] [ password(optinal) ]"
     exit 1
 fi
