344e295819f2c749e3b7640a020019dd10314ac1
David Rabkin <david@rabkin.co.il>
2023-11-07 23:01:51 +0200

    Add shell linters and fix all errors (#59)

--- scripts/send-to-collector.sh
+++ scripts/send-to-collector.sh
@@ -7,13 +7,13 @@ partnerName="$4"
 decodedPassword="$5"
 
 if [ -z "$endpoint" ] || [ -z "$claimFile" ] || [ -z "$executedBy" ]; then
-    echo "Usage: $0 [ endpoint ] [ path/to/claim.json ] [ executed_by ] [ partner_name(optional ] [ password(optinal) ]"
-    exit 1
+	echo "Usage: $0 [ endpoint ] [ path/to/claim.json ] [ executed_by ] [ partner_name(optional ] [ password(optinal) ]"
+	exit 1
 fi
 
-curl -X POST $endpoint \
-    -H "Content-Type: multipart/form-data" \
-    -F "claimFile=@$claimFile" \
-    -F "executed_by=$executedBy" \
-    -F "partner_name=$partnerName" \
-    -F "decoded_password=$decodedPassword"
+curl -X POST "$endpoint" \
+	-H "Content-Type: multipart/form-data" \
+	-F "claimFile=@$claimFile" \
+	-F "executed_by=$executedBy" \
+	-F "partner_name=$partnerName" \
+	-F "decoded_password=$decodedPassword"
