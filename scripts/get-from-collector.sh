322374851492db5c96e24a354f5deb645322fa01
David Rabkin <david@rabkin.co.il>
2023-11-07 14:55:46 +0200

    Add space to shell condition (#58)

--- scripts/get-from-collector.sh
+++ scripts/get-from-collector.sh
@@ -4,7 +4,7 @@ endpoint="$1"
 partnerName="$2"
 decodedPassword="$3"
 
-if [ -z "$endpoint" ] || [ -z "$partnerName" ] || [ -z "$decodedPassword"]; then
+if [ -z "$endpoint" ] || [ -z "$partnerName" ] || [ -z "$decodedPassword" ]; then
     echo "Usage: $0 [ endpoint ] [ partner_name ] [ password ]"
     exit 1
 fi
