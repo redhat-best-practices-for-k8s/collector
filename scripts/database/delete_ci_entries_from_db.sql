91456a91cb27a12b8a3a32aea88fb1289358437a
Shir Moran <101132224+shirmoran@users.noreply.github.com>
2024-01-23 17:45:56 +0200

    cron for deleting CI entries on a weekly cadence (#128)

---
+++ scripts/database/delete_ci_entries_from_db.sql
@@ -0,0 +1,17 @@
+USE cnf;
+
+SET GLOBAL event_scheduler = ON;
+
+CREATE EVENT delete_ci_entries_from_claim_result
+ON SCHEDULE EVERY 24 HOUR ENABLE
+DO DELETE FROM claim_result
+WHERE claim_id IN (
+    SELECT id
+    FROM claim
+    WHERE executed_by = 'CI' AND upload_time < DATE_SUB(NOW(), INTERVAL 1 WEEK)
+);
+
+CREATE EVENT delete_ci_entries_from_claim
+ON SCHEDULE EVERY 24 HOUR ENABLE
+DO DELETE FROM claim
+WHERE executed_by = 'CI' AND upload_time < DATE_SUB(NOW(), INTERVAL 1 WEEK);
