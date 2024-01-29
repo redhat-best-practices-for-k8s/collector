7935bd414d3b89e619d00ec8b15b40f1cdba4a11
Shir Moran <101132224+shirmoran@users.noreply.github.com>
2024-01-29 18:39:58 +0200

    cron for deleting ci users from authenticator table (#140)

--- scripts/database/delete_ci_entries_from_db.sql
+++ scripts/database/delete_ci_entries_from_db.sql
@@ -11,6 +11,15 @@ WHERE claim_id IN (
     WHERE executed_by = 'CI' AND upload_time < DATE_SUB(NOW(), INTERVAL 1 WEEK)
 );
 
+CREATE EVENT delete_ci_entries_from_authenticator
+ON SCHEDULE EVERY 1 MINUTE ENABLE
+DO DELETE FROM authenticator
+WHERE partner_name IN (
+    SELECT partner_name
+    FROM claim
+    WHERE executed_by = 'CI' AND upload_time < DATE_SUB(NOW(), INTERVAL 1 MINUTE)
+);
+
 CREATE EVENT delete_ci_entries_from_claim
 ON SCHEDULE EVERY 24 HOUR ENABLE
 DO DELETE FROM claim
